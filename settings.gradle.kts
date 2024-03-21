1071a658d4eef8d1694ba9ca516efe01c7eeafb9
Darrenk <s160421024@student.ubaya.ac.id>
2024-03-21 16:27:00 +0700

    new

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "advWeek4160421024"
+include(":app")
+ 
