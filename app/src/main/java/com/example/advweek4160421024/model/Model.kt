31b4a0bb20814656f864dfc4a2636331674f2b6f
Darrenk <s160421024@student.ubaya.ac.id>
2024-03-24 19:38:46 +0700

    new2

---
+++ app/src/main/java/com/example/advweek4160421024/model/Model.kt
@@ -0,0 +1,9 @@
+package com.example.advweek4160421024.model
+
+data class Student(
+    var id : String?,
+    var name:String?,
+    var dob:String?,
+    var phone:String?,
+    var photoUrl:String?,
+)
